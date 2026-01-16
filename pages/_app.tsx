eb04beac7aa37e549a20ccc9d7b6269c9a5b497b
Patrick Mûtwîri <dev@patric.xyz>
2026-01-16 14:27:11 +0400

    feat: add @vercel/speed-insights dependency and integrate into the application layout
    - Added @vercel/speed-insights version 1.3.1 to package.json and package-lock.json.
    - Integrated SpeedInsights component into the main application layout for performance monitoring.
    
    This enhancement improves the application's performance tracking capabilities.
    
    Signed-off-by: Patrick Mûtwîri <dev@patric.xyz>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from '@/components/Layout'
 import { Analytics } from "@vercel/analytics/next"
 import '@vercel/examples-ui/globals.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -20,6 +21,7 @@ function App({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </Layout>
     </SessionProvider>
   )
